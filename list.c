ef8cdacb455ea2bb3f692dbca72a94d1ab74ab12
Oleh SAMOILENKO <osamoile@e2r5p9.unit.ua>
2018-04-03 19:39:19 +0300

    lists

--- list.c
+++ list.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "header.h"
 
+t_tetro		*lstnew(int *x, int *y, char c)
+{
+	t_tetro		*new;
+
+	new = (t_tetro*)malloc(sizeof(t_tetro));
+	int i = -1;
+	while (++i < 4)
+	{
+		new->x[i] = x[i];
+		new->y[i] = y[i];
+	}
+	new->symbol = c;
+	new->next = 0;
+	return (new);
+}
+
+void		lstadd(t_tetro *lst, int *x, int *y, char c)
+{
+	while (lst->next)
+		lst = lst->next;
+	lst->next = lstnew(x, y, c);
+}
